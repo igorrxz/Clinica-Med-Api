09abac4d6cfa1b855322aceba0424e5639584ab4
Xima <igorrx24072004@gmail.com>
2024-02-27 16:00:28 -0300

    Listagem de Pacientes

---
+++ api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,9 @@
+package med.voll.api.paciente;
+
+public record DadosListagemPaciente(String nome, String cpf, String email, String telefone) {
+
+    public DadosListagemPaciente(Paciente paciente){
+        this(paciente.getNome(), paciente.getCpf(), paciente.getEmail(), paciente.getTelefone());
+    }
+
+}
