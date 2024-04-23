c03a4dce7f9899244694bed2b081ab2223f9ba85
Xima <igorrx24072004@gmail.com>
2024-04-23 15:26:55 -0300

    Create Delete Requisition from Medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -38,5 +38,11 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluirMedicos(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
 
