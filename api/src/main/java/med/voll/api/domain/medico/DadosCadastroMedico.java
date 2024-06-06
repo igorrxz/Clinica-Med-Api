75d290d0b6586f48aaed53ba16ef52bd35e1a880
Xima <igorrx24072004@gmail.com>
2024-06-06 14:14:11 -0300

    Tratamento de Erro 400

--- api/src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -1,32 +1,29 @@
 package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
 
-        @NotBlank
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
 
-        @NotBlank
-        @Email
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Formato do email é inválido")
         String email,
 
-        @NotBlank
+        @NotBlank(message = "Telefone é obrigatório")
         String telefone,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{4,6}")
+        @NotBlank(message = "CRM é obrigatório")
+        @Pattern(regexp = "\\d{4,6}", message = "Formato do CRM é inválido")
         String crm,
 
-        @NotNull
+        @NotNull(message = "Especialidade é obrigatória")
         Especialidade especialidade,
 
-        @NotNull
-        @Valid
-        DadosEndereco endereco) {
-}
+        @NotNull(message = "Dados do endereço são obrigatórios")
+        @Valid DadosEndereco endereco
+)
+{}
